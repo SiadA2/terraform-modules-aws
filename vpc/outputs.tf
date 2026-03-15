af707d9d1f9a34e81d280ce4240f8f29322a4540
siad <siadadam2@gmail.com>
2026-03-15 16:02:20 +0000

    add public and private route tables

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -17,3 +17,18 @@ output "availability_zones" {
   description = "List of availability zones used"
   value       = var.availability_zones
 }
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.private.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.gw.id
+}
