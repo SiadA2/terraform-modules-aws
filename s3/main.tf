71e5a1357f75504e64170570361afd34e18ec6cc
siad <siadadam2@gmail.com>
2026-03-16 22:53:54 +0000

    add: s3 bucket with versioning & disabled public access

---
+++ s3/main.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "buckets" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.buckets.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+
+  bucket = aws_s3_bucket.buckets.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
