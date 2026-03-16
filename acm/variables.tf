38b2ca7fac7fb641c397663f830cbe5ec71b0fc9
siad <siadadam2@gmail.com>
2026-03-16 02:19:56 +0000

    add: acm with validation

---
+++ acm/variables.tf
@@ -0,0 +1,15 @@
+variable "domain_name" {
+  type = string
+  description = "Domain name of the Route 53 hosted zone"
+}
+
+variable "validation_method" {
+  type = string
+  description = "How the ACM validates your domain"
+  default = "DNS"
+}
+
+variable "hosted_zone_id" {
+  type = string
+  description = "ID of the Route 53 hosted zone"
+}
