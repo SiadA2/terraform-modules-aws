764a92bff144b98080b3a562e41470b3c06e1a04
siad <siadadam2@gmail.com>
2026-03-16 01:18:35 +0000

    add: ecs security group

--- security-grps/main.tf
+++ security-grps/main.tf
@@ -25,3 +25,25 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Traffic to the ECS cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = var.ecs_security_group_name
+  description = var.ecs_security_group_desc
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = [aws_security_group.lb.id]
+
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
