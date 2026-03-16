807a8f6beaaa36c3705c070c438d7e5d4e7ce74a
siad <siadadam2@gmail.com>
2026-03-16 02:01:47 +0000

    add: ecr repository with lifecycle policies

---
+++ ecr/main.tf
@@ -0,0 +1,44 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = var.repo_name
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_lifecycle_policy" "untagged" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Expire images older than 14 days"
+      selection = {
+        tagStatus     = "untagged"
+        countType     = "sinceImagePushed"
+        countUnit     = "days"
+        countNumber   = 14
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_lifecycle_policy" "tagged" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+      selection = {
+        tagStatus       = "tagged"
+        tagPrefixList   = ["v"]
+        countType       = "imageCountMoreThan"
+        countNumber     = 30
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
