8d33440f7cf8cdc7fd7a8a3ec7e19277488ac45b
siad <siadadam2@gmail.com>
2026-03-24 13:44:37 +0000

    add: https listener

--- alb/main.tf
+++ alb/main.tf
@@ -4,3 +4,16 @@ resource "aws_alb" "main" {
   subnets         = var.public_subnets_id
   security_groups = var.security_groups
 }
+
+# Forward HTTPS traffic to the target group
+resource "aws_alb_listener" "https" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = ""
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.arn
+    type             = "forward"
+  }
+}
