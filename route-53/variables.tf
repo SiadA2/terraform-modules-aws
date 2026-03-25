666675ee488e94351e8e3f66c65632ce993c3895
SiadA2 <siadadam2@gmail.com>
2026-03-25 22:08:16 +0000

    mv: acm resources to route-53 and rm: acm module

--- route-53/variables.tf
+++ route-53/variables.tf
@@ -2,3 +2,19 @@ variable "domain_name" {
   type        = string
   description = "The domain name for the Route 53 hosted zone"
 }
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name of the Route 53 hosted zone"
+}
+
+variable "validation_method" {
+  type        = string
+  description = "How the ACM validates your domain"
+  default     = "DNS"
+}
+
+variable "hosted_zone_id" {
+  type        = string
+  description = "ID of the Route 53 hosted zone"
+}
